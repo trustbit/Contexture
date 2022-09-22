2421cf3002aac3c6fd413934f023b11411a3628f
Sebastian Burgstaller <sebastian.burgstaller@trustbit.tech>
2022-09-22 06:47:08 +0200

    Implemented short name validation changes (#104)

--- frontend/src/ShortName.elm
+++ frontend/src/ShortName.elm
@@ -14,7 +14,10 @@ type ShortName =
 
 type Problem
   = Empty
+  | ExceedsMaxLength
   | StartsWithNumber
+  | StartsWithHyphen
+  | EndsWithHyphen
   | ContainsWhitespace
   | ContainsSpecialChars (Set Char)
 
@@ -23,13 +26,30 @@ invalidWhitespace =
   [ ' ', '\n', '\r' ]
   |> Set.fromList
 
+isHyphen : Char -> Bool
+isHyphen char =
+  char == '-'
+
 checkNonEmpty : List Char -> Result Problem ShortName
 checkNonEmpty chars =
   let
+    exceedsMaxLength =
+      List.length chars > 16
     startsWithNumber =
       chars
       |> List.head
       |> Maybe.map Char.isDigit
+      |> Maybe.withDefault False
+    startsWithHyphen =
+      chars
+      |> List.head
+      |> Maybe.map isHyphen
+      |> Maybe.withDefault False
+    endsWithHyphen =
+      chars
+      |> List.reverse
+      |> List.head
+      |> Maybe.map isHyphen
       |> Maybe.withDefault False
     containsWhitespaces =
       invalidWhitespace
@@ -40,10 +60,17 @@ checkNonEmpty chars =
       chars
       |> Set.fromList
       |> Set.filter (Char.isAlphaNum >> not)
+      |> Set.filter (isHyphen >> not)
 
   in
-    if startsWithNumber then
+    if exceedsMaxLength then
+      Err ExceedsMaxLength
+    else if startsWithNumber then
       Err StartsWithNumber
+    else if startsWithHyphen then
+      Err StartsWithHyphen
+    else if endsWithHyphen then
+      Err EndsWithHyphen
     else if containsWhitespaces then
       Err ContainsWhitespace
     else if not (Set.isEmpty specialChars) then
