0999c1acca7fd50e0423a592d49fb7be788f11e9
Thomas Burko <thomas.burko@trustbit.tech>
2023-08-25 11:25:41 +0200

    chore(e2e): run e2e tests in preview instead of dev (disabled HMR)

--- frontend-vue/playwright.config.ts
+++ frontend-vue/playwright.config.ts
@@ -86,7 +86,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run preview",
     port: 4200,
     reuseExistingServer: true,
   },
