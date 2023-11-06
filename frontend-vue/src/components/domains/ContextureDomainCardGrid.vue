4cf1cdaec668556dc591e5d25e5211b673108fe3
Jorge Esteves <jota.steves@gmail.com>
2023-11-06 16:46:33 +0100

    feat: add default on to show switch, add sticky

--- frontend-vue/src/components/domains/ContextureDomainCardGrid.vue
+++ frontend-vue/src/components/domains/ContextureDomainCardGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="domains.length" class="mb-4">
+  <div v-if="domains.length" class="sticky top-0 mb-4 border border-x-0 border-t-0 border-blue-100 bg-white pb-2">
     <ContextureSwitch
       v-model="options.showBadges"
       :label="t('domains.details.filter.show_bounded_contexts_and_subdomains')"
@@ -38,6 +38,6 @@ withDefaults(defineProps<Props>(), {
 
 const { t } = useI18n();
 const options: RemovableRef<DomainCardGridOptions> = useLocalStorage<DomainCardGridOptions>("settings.domains.grid", {
-  showBadges: false,
+  showBadges: true,
 });
 </script>
