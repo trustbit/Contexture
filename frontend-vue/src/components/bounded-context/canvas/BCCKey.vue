46b2925539a0e817a90e915360f1ebe75749368c
K-Dud <krystian.dudzik@veroo.io>
2024-07-01 15:24:16 +0200

    BoundedContext short name validation
    Changes scope of uniqueness constraint for bounded context short name
    Fixes bug where short name constraint wasn't checked correctly

--- frontend-vue/src/components/bounded-context/canvas/BCCKey.vue
+++ frontend-vue/src/components/bounded-context/canvas/BCCKey.vue
@@ -10,7 +10,7 @@
     <div v-if="editMode">
       <ContextureHelpfulErrorAlert v-if="submitError" v-bind="submitError" class="mb-4" />
       <Form @submit="onUpdate">
-        <ContextureChangeKey v-model="key" />
+        <ContextureInputText v-model="key" :rules="boundedContextShortNameValidator" />
 
         <ContexturePrimaryButton type="submit" :label="t('common.save')" class="mt-4" size="sm">
           <template #left>
@@ -27,28 +27,38 @@
 </template>
 
 <script setup lang="ts">
+import { toFieldValidator } from "@vee-validate/zod";
 import { storeToRefs } from "pinia";
 import { Form } from "vee-validate";
 import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import ContextureBoundedContextCanvasElement from "~/components/bounded-context/canvas/ContextureBoundedContextCanvasElement.vue";
-import ContextureChangeKey from "~/components/core/change-short-name/ContextureChangeShortName.vue";
+import { boundedContextShortNameValidationSchema } from "~/components/core/change-short-name/changeShortNameValidationSchema";
+("~/components/core/change-short-name/changeShortNameValidationSchema");
 import ContextureHelpfulErrorAlert, {
   HelpfulErrorProps,
 } from "~/components/primitives/alert/ContextureHelpfulErrorAlert.vue";
 import ContexturePrimaryButton from "~/components/primitives/button/ContexturePrimaryButton.vue";
+import ContextureInputText from "~/components/primitives/input/ContextureInputText.vue";
 import { useAuthStore } from "~/stores/auth";
 import { useBoundedContextsStore } from "~/stores/boundedContexts";
 import IconsMaterialSymbolsFormatKeyOutline from "~icons/material-symbols/key-outline";
 
 const icon = IconsMaterialSymbolsFormatKeyOutline;
 const store = useBoundedContextsStore();
-const { activeBoundedContext } = storeToRefs(store);
+const { activeBoundedContext, boundedContextsByDomainId } = storeToRefs(store);
 const { canModify } = useAuthStore();
 const { t } = useI18n();
 const key = ref(activeBoundedContext.value.shortName);
 const submitError = ref<HelpfulErrorProps | undefined>();
 const editMode = ref(false);
+const boundedContextShortNameValidator = toFieldValidator(
+  boundedContextShortNameValidationSchema(
+    boundedContextsByDomainId.value[activeBoundedContext.value.parentDomainId].filter(
+      (bc) => bc.id !== activeBoundedContext.value.id
+    )
+  )
+);
 
 async function onUpdate() {
   submitError.value = null;
